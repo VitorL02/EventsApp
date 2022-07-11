f7d465ce11b1e8447e0e4eaa5ecd985145720c7c
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-07-11 17:20:59 -0300

    Finalizando a home page
    Alterando os arquivos da home page e adicionando o html/css da home, juntamente com a mudança de fav icon

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,12 +3,28 @@
 @section('title','EventsApp - Home Page')
 
 @section('content')
-
-        <h1>Titulo Teste</h1>
-        <img src="/img/banner.jpg" alt="">
-        @foreach($events as $event)
-        <!-- Utilizando uma -> e possivel acessar as propriedades de um objeto -->
-        <p>{{$event->title}} -- {{$event->description}}</p> 
-    @endforeach
+    <div id="search-container" class="col-md-12">
+        <h1>Busque um Evento</h1>
+        <form action="">
+            <input type="search" name="search" id="search" class="form-control" placeholder ="Busque...">
+        </form>
+    </div>
+    <div id="events-container" class="col-md-12">
+        <h2>Proximos Eventos :</h2>
+        <p class="subtitle">Veja os eventos programados</p>
+        <div id="cards-container" class="row">
+            @foreach($events as $event)
+                <div class="card col-md-3">
+                    <img src="/img/event_placeholder.jpg" alt="{{$event->title}}">
+                    <div class="card-body">
+                        <p class="card-date">10/01/2001</p>
+                        <h5 class="card-title">{{$event->title}}</h5>
+                        <p class="card-participants">X Participantes</p>
+                        <a href="#" class ="btn btn-primary">Saber Mais</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 
 @endsection
