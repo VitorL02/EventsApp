443ec106c5630d8d499ac2504e23f19300a84b1c
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-07-11 17:21:29 -0300

    Adicionando fav icon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -40,7 +40,7 @@
 
         @yield('content')
     <footer>
-        <p>EventsApp - Curso Laravel Hora de Co dar  &copy; 2022</p>
+        <p>EventsApp - Curso Laravel Hora de Codar  &copy; 2022</p>
     </footer>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
