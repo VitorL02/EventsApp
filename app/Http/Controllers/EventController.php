3966c262782c827cc31b138ccaf6958d34b6c117
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-07-12 11:53:49 -0300

    Salvando os dados passados pelo form de criar evento no banco e criando a pagina de formulario
    Utilizando o arquivo de rotas uma função store foi criada pra salvar os dados no banco diretamente via post

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -16,4 +16,16 @@ class EventController extends Controller
     public function createEvents(){
         return view('events.create');
     }
+    public function store(Request $request){
+        $event = new Event;
+        //Acessa os dados do objeto instaciado na view e salva no banco com o metodo save
+        $event->title=$request->title;
+        $event->city=$request->city;
+        $event->private=$request->private;
+        $event->description=$request->description;
+        
+        $event->save();
+
+        return redirect('/');
+    }
 }
